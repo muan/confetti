a90dc3fe98fd604da513932a218403c822b3d9e0
Mu-An Chiou <me@muanchiou.com>
2016-03-20 18:48:11 -0700

    :tada:

---
+++ index.js
@@ -0,0 +1,34 @@
+var menubar = require('menubar')
+var ipc = require('electron').ipcMain
+
+var mb = menubar({
+  transparent: true,
+  width: 100,
+  height: 100,
+  tooltip: 'Who\'s awesome?!'
+})
+
+mb.on('ready', function () {
+  var size = require('electron').screen.getPrimaryDisplay().workAreaSize
+  mb.setOption('width', size.width)
+  mb.setOption('height', size.height)
+})
+
+mb.on('show', function () {
+  mb.window.webContents.send('start-countdown')
+})
+
+mb.on('after-create-window', function () {
+  mb.window.webContents.on('did-finish-load', function () {
+    mb.window.webContents.send('start-countdown')
+  })
+})
+
+mb.on('hide', function () {
+  mb.window.webContents.send('stop-countdown')
+})
+
+// when receive the abort message, close the app
+ipc.on('abort', function () {
+  mb.hideWindow()
+})
